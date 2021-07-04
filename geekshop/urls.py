0f00a865d75e41ee47ca6bfb21e1811840243fa2
Yuriy <PolovnevYA@gmail.com>
2021-07-04 18:12:08 +0300

    add views.py login and registration fix templates

--- geekshop/urls.py
+++ geekshop/urls.py
@@ -21,6 +21,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('products.urls', namespace='products')),
+    path('users/', include('users.urls', namespace='users')),
 ]
 
 if settings.DEBUG:
