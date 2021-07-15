f3f3374152e2c8d782adbe99953fa192184237c7
Yuriy <PolovnevYA@gmail.com>
2021-07-15 21:33:52 +0300

    fix

--- adminapp/urls.py
+++ adminapp/urls.py
@@ -13,18 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
 
+from django.urls import path
+from .views import login, registration, logout, profile
+
+app_name = 'adminapp'
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('products.urls', namespace='products')),
-    path('users/', include('users.urls', namespace='users')),
-    path('baskets/', include('baskets.urls', namespace='baskets')),
-    path('adminapp/', include('adminapp.urls', namespace='adminapp')),
+    path('login/', login, name='login'),
+    path('registration/', registration, name='registration'),
+    path('profile/', profile, name='profile'),
+    path('logout/', logout, name='logout'),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- geekshop/settings.py
+++ geekshop/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'products',
     'users',
     'baskets',
+    'adminapp',
+
 ]
 
 MIDDLEWARE = [
